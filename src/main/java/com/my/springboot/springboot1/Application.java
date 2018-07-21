3aee0217983203ae1094407428fe8716e873c075
wugy <wu.gongyao@puscene.com>
2018-07-21 18:07:02 +0800

    thymeleaf config

--- src/main/java/com/my/springboot/springboot1/Application.java
+++ src/main/java/com/my/springboot/springboot1/Application.java
@@ -2,6 +2,7 @@ package com.my.springboot.springboot1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -10,8 +11,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 ////启用定时任务
 //@EnableScheduling
 
-
-@SpringBootApplication
+//默认加载了thymeleaf 配置 如若不配置thymeleaf包则报错，所以不适用可以排除掉
+@SpringBootApplication(exclude = ThymeleafAutoConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
