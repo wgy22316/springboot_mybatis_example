b37b66be5c1127bfbabcce675fddfa64f6e8732d
wugy <wu.gongyao@puscene.com>
2018-09-06 19:47:55 +0800

    shardingjdbc

--- src/main/java/com/my/springboot/springboot1/Application.java
+++ src/main/java/com/my/springboot/springboot1/Application.java
@@ -1,11 +1,19 @@
 package com.my.springboot.springboot1;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.util.ResourceUtils;
 import tk.mybatis.spring.annotation.MapperScan;
+
+import javax.activation.DataSource;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
 
 @MapperScan(basePackages = "com.my.springboot.springboot1.mapper")
 ////启用定时任务
@@ -18,4 +26,5 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
 }
